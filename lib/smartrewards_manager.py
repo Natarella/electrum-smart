dcf793a974b92b8c86805b61b6d5d41d190bb838
Solarminer <solarminer@smartcash.cc>
2021-01-01 23:39:52 -0600

    Estimated Rewards Fix

--- lib/smartrewards_manager.py
+++ lib/smartrewards_manager.py
@@ -125,7 +125,7 @@ class SmartrewardsManager(object):
         for reward in self.rewards_addresses:
             if addr == reward.address:
                 reward.eligible_amount = eligible_balance
-                reward.estimated_reward = float("{0:.8f}".format(eligible_balance * self.rewards_info.estimated_percent))
+                reward.estimated_reward = float("{0:.8f}".format(eligible_balance * 0.01 * self.rewards_info.estimated_percent))
                 break
 
         print_msg('Rewards for [{}] is [{}]'.format(addr, eligible_balance))
